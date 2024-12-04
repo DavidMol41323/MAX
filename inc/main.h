7ff593ae34de8a1c6c2a4b187cd4492b0e7bf859
David Moler <mol41323@spseol.cz>
2024-12-04 09:11:45 +0100

    funkce max7219_send

--- inc/main.h
+++ inc/main.h
@@ -8,8 +8,9 @@
 #define READ(BAGR) (GPIO_ReadInputPin(BAGR##_PORT, BAGR##_PIN)!=RESET) 
 #define PUSH(BAGR) (GPIO_ReadInputPin(BAGR##_PORT, BAGR##_PIN)==RESET) 
 
-#define DIN_PORT GPIOC
-#define DIN_PIN GPIO_PIN_5
+//Definování portů
+#define DATA_PORT GPIOC
+#define DATA_PIN GPIO_PIN_5
 
 #define CS_PORT GPIOC
 #define CS_PIN GPIO_PIN_7
@@ -17,7 +18,39 @@
 #define CLK_PORT GPIOC
 #define CLK_PIN GPIO_PIN_6
 
+//definování HIGH a LOW
+#define CLK_HIGH GPIO_WriteHigh(CLK_GPIO, CLK_PIN)
+#define CLK_LOW GPIO_WriteLow(CLK_GPIO, CLK_PIN)
+#define DATA_HIGH GPIO_WriteHigh(DIN_GPIO, DATA_PIN)
+#define DATA_LOW GPIO_WriteLow(DIN_GPIO, DATA_PIN)
+#define CS_HIGH GPIO_WriteHigh(CS_GPIO, CS_PIN)
+#define CS_LOW GPIO_WriteLow(CS_GPIO, CS_PIN)
 
+// makra adres/příkazů pro čitelnější ovládání MAX7219
+#define NOOP 		 0   // No operation
+#define DIGIT0 		 1	 // zápis hodnoty na 1. cifru
+#define DIGIT1 		 2	 // zápis hodnoty na 2. cifru
+#define DIGIT2 		 3	 // zápis hodnoty na 3. cifru
+#define DIGIT3 		 4	 // zápis hodnoty na 4. cifru
+#define DIGIT4 		 5	 // zápis hodnoty na 5. cifru
+#define DIGIT5 		 6	 // zápis hodnoty na 6. cifru
+#define DIGIT6 		 7	 // zápis hodnoty na 7. cifru
+#define DIGIT7 		 8	 // zápis hodnoty na 8. cifru
+#define DECODE_MODE  9	 // Aktivace/Deaktivace znakové sady (my volíme vždy hodnotu DECODE_ALL)
+#define INTENSITY 	 10	 // Nastavení jasu - argument je číslo 0 až 15 (větší číslo větší jas)
+#define SCAN_LIMIT 	 11	 // Volba počtu cifer (velikosti displeje) - argument je číslo 0 až 7 (my dáváme vždy 7)
+#define SHUTDOWN 	 12	 // Aktivace/Deaktivace displeje (ON / OFF)
+#define DISPLAY_TEST 15	 // Aktivace/Deaktivace "testu" (rozsvítí všechny segmenty)
+
+// argumenty pro SHUTDOWN
+#define DISPLAY_ON		1	// zapne displej
+#define DISPLAY_OFF		0	// vypne displej
+// argumenty pro DISPLAY_TEST
+#define DISPLAY_TEST_ON 	1	// zapne test displeje
+#define DISPLAY_TEST_OFF 	0	// vypne test displeje
+// argumenty pro DECODE_MOD
+#define DECODE_ALL		0b11111111 // (lepší zápis 0xff) zapíná znakovou sadu pro všechny cifry
+#define DECODE_NONE		0 // vypíná znakovou sadu pro všechny cifry
 
 // Discovery Board
 #ifdef STM8S003
--- src/main.c
+++ src/main.c
@@ -10,28 +10,58 @@ void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
-    GPIO_Init(DIN_PORT, DIN_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DATA_PORT, DATA_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(CS_PORT,CS_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(CLK_PORT, CLK_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+
+    // nastavíme základní parametry budiče
+    max7219_posli(DECODE_MODE, DECODE_ALL); // zapnout znakovou sadu na všech cifrách
+    max7219_posli(SCAN_LIMIT, 7); // velikost displeje 8 cifer (počítáno od nuly, proto je argument číslo 7)
+    max7219_posli(INTENSITY, 5); // volíme ze začátku nízký jas (vysoký jas může mít velkou spotřebu - až 0.25A !)
+    max7219_posli(DISPLAY_TEST, DISPLAY_TEST_OFF); // Funkci "test" nechceme mít zapnutou
+    max7219_posli(SHUTDOWN, DISPLAY_ON); // zapneme displej
 
     init_milis();
     //init_uart1();
 }
 
+void max7219_send(uint8_t adress, uint8_t data){
+    uint8_t maska;
+    CS_LOW; //Nastavení LOAD na low
+
+    maska= 0b100000000; //Odeslání prvních 8 bitů (adresa)
+    CLK_LOW; //Nastavení CLK na low
+
+    while(maska){ //dokud jsme neposlali všech 8 bitů
+        if(maska & adress){ //Pokud je právě vysílaný bit 1
+        DATA_HIGH;
+        }
+        else{ //Pokud je právě vysílaný bit 0
+            DATA_LOW;
+        }
+        CLK_HIGH;
+        maska= maska>>1 // rotujeme masku abychom v příštím kroku vysílali nižší bit
+        CLK_LOW
+    }
+    maska= 0b10000000; //Odeslání dolních 8 bitů (data)
+    while(maska){ //dokud jsme neposlali všech 8 bitů
+        if(maska & adress){ //Pokud je právě vysílaný bit 1
+        DATA_HIGH;
+        }
+        else{ //Pokud je právě vysílaný bit 0
+            DATA_LOW;
+        }
+        CLK_HIGH;
+        maska= maska>>1 // rotujeme masku abychom v příštím kroku vysílali nižší bit
+        CLK_LOW
+    }
+    CS_HIGH;
+}
 
 int main(void)
 {
-
     init();
-
-    while (1) {
-
-
-
-    }
+    max7219_send(DIGIT0, 2)
 }
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
